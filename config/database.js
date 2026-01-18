07d680f799a233ca458031c1a8c7c8f7f025d184
securelucifer <secure.lucifer.pandey@gmail.com>
2026-01-18 21:52:28 +0530

    hhh

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "Dmart",
+            dbName: "DmartUpi",
         });
 
         console.log(`📊 MongoDB Connected: ${conn.connection.host}`);
